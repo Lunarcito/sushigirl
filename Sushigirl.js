f28b807037054083f699ddc3b9651479943811b4
Carolina Calle <calle.carolina@gmail.com>
2022-10-19 12:31:31 +0200

    lifes added

--- Sushigirl.js
+++ Sushigirl.js
@@ -1,8 +1,12 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
-const imageMaki = new Image ();
-imageMaki.src = "./images/sushigirl.png";
+const sushigirl = new Image ();
+sushigirl.src = "./images/sushigirl.png";
+
+const maki = new Image ();
+maki.src = "./images/maki.png"
+
 
 const onigiri = new Image ();
 onigiri.src = "./images/onigiri2.png";
@@ -11,6 +15,7 @@ const wasabi = new Image ();
 wasabi.src = "./images/wasabi.png";
 
 let score = 0
+let lifes = 3
 let stopGame = false
 let obstaculosArray = []
 
@@ -24,17 +29,17 @@ function startGame(){
     update ()
 }
 
-class Maki {
+class Sushi {
     constructor (){
         this.x = 70
-        this.y = 250
+        this.y = 300
         this.speed= 1
         this.w = 70
         this.h = 70
         
     }
-        drawMaki (){
-            ctx.drawImage (imageMaki,this.x,this.y,this.w,this.h)
+        drawSushi (){
+            ctx.drawImage (sushigirl,this.x,this.y,this.w,this.h)
         }
     
         moveUp (){
@@ -57,24 +62,24 @@ class Maki {
           }
     }
 
-    let maki = new Maki
+    let sushi = new Sushi
 
 document.addEventListener ("keydown",(e)=> {
     console.log(e);
     if (e.keyCode === 38){
-        maki.moveUp ()
-        maki.drawMaki ()
+        sushi.moveUp ()
+        sushi.drawSushi ()
     } else if (e.keyCode === 40){
-        maki.moveDown ()
-        maki.drawMaki ()
+        sushi.moveDown ()
+        sushi.drawSushi ()
     }
 })
 
 class Obstaculo {
     constructor (tipo){
-        this.x = 700
-        this.y= Math.random ()*canvas.height
-        this.speed = 3
+        this.x = 800
+        this.y= Math.random ()*canvas.height-100
+        this.speed = 4
         this.w =70
         this.h =70
         this.tipo = tipo
@@ -87,48 +92,67 @@ class Obstaculo {
             ctx.drawImage (wasabi,this.x,this.y,this.w,this.h)
         } else if(this.tipo === "onigiri"){
             ctx.drawImage (onigiri,this.x,this.y,this.w,this.h)
-        }
+        } else if (this.tipo === "maki"){
+            ctx.drawImage (maki,this.x,this.y,this.w,this.h)
+       }
     }
 }
 
  setInterval(()=> {
-    let n= Math.floor(Math.random()*2)
+    let n= Math.floor(Math.random()*3)
     if(n===0){
         obstaculosArray.push(new Obstaculo("onigiri"))
     } else if(n===1){
         obstaculosArray.push(new Obstaculo("wasabi"))
+    } else if(n===2){
+        obstaculosArray.push(new Obstaculo("maki"))
     }
-},3000)
+},1500)
   
 
 const drawScore=()=>{
     ctx.font = "20px Arial";
-    ctx.fillStyle = "blue";
-    ctx.fillText(score,canvas.width/2,200);
+    ctx.fillStyle = "purple";
+    ctx.fillText("Score: "+ score,750,150);
+  }
+const drawLifes=()=>{
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "brown";
+    ctx.fillText("Lives: "+lifes,750,100);
   }
   
   const drawGameOver=()=>{
     let gameOver= "Game Over!"
     ctx.font = "40px Arial";
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "purple";
     ctx.fillText(gameOver +`Your final score is ${score}`,100,100)
   }
 
   const update = () => {
     if (!stopGame) {
         ctx.clearRect (0,0,canvas.width,canvas.height)
-        maki.drawMaki();
+        sushi.drawSushi();
         for (let i =0; i< obstaculosArray.length;i++){
             obstaculosArray[i].drawObstaculo()
-            if(maki.contains(obstaculosArray[i])){
-                if (obstaculosArray[i].tipo ==="wasabi"){
-                    stopGame = true;
-                    drawGameOver ()
+            if(sushi.contains(obstaculosArray[i])){
+                if (obstaculosArray[i].tipo ==="wasabi"&& obstaculosArray[i].collition === false){
+                    if(lifes >1){
+                    lifes--;  
+                    obstaculosArray[i].collition=true
+                    } else {
+                        lifes--;
+                        return drawGameOver ()
+                    }
+                
                 } else if(obstaculosArray[i].tipo === "onigiri" && obstaculosArray[i].collition === false){
-                    score+=100
+                    score+=100                    
+                    obstaculosArray[i].collition=true
+                } else if(obstaculosArray[i].tipo === "maki" && obstaculosArray[i].collition === false){
+                    score+=500;
                     obstaculosArray[i].collition=true
                 }
             }
+            drawLifes ()
             drawScore()
         }
         requestAnimationFrame(update)
